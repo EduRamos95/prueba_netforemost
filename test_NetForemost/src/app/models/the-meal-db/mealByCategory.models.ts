821a29fee72517ed53bc77f8f40f071c82a9a1fd
EduRamos95 <eduramos08.30@gmail.com>
2024-07-22 18:45:00 -0500

    update: interfaces

--- test_NetForemost/src/app/models/the-meal-db/mealByCategory.models.ts
+++ test_NetForemost/src/app/models/the-meal-db/mealByCategory.models.ts
@@ -1,5 +1,10 @@
 export interface SearchMeal {
   strMeal: string;
   strMealThumb: string;
+  strCategory:string;
   idMeal: string;
+}
+
+export interface SearchMealResponse {
+  meals: SearchMeal[];
 }
