ac83e0e19e95e2797cce6120886371def59007f5
EduRamos95 <eduramos08.30@gmail.com>
2024-07-22 18:45:55 -0500

    update: visualize data search by name

--- test_NetForemost/src/app/views/pages/home/components/card-meal/card-meal.component.ts
+++ test_NetForemost/src/app/views/pages/home/components/card-meal/card-meal.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-card-meal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './card-meal.component.html',
   styleUrl: './card-meal.component.scss'
 })
 export class CardMealComponent {
-
+  @Input() name:string = "";
+  @Input() img:string = "";
+  @Input() category:string = "";
 }
