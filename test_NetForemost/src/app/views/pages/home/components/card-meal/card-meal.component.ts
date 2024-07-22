e8091c6639c3819283f5a18b02cc875009b15e0d
EduRamos95 <eduramos08.30@gmail.com>
2024-07-22 17:36:32 -0500

    create: card for meal

---
+++ test_NetForemost/src/app/views/pages/home/components/card-meal/card-meal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-meal',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-meal.component.html',
+  styleUrl: './card-meal.component.scss'
+})
+export class CardMealComponent {
+
+}
