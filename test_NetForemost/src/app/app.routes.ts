9cd6980bf301d8463ae8f88641e8df8c9c8d14ec
EduRamos95 <eduramos08.30@gmail.com>
2024-07-22 17:35:57 -0500

    update: routing page

--- test_NetForemost/src/app/app.routes.ts
+++ test_NetForemost/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'index',
+        loadChildren: () => import('./views/pages/home/home.routes').then(c => c.HOME_ROUTES)
+    },
+    {
+        path: 'details',
+        loadChildren: () => import('./views/pages/details/details.routes').then(c => c.DETAILS_ROUTES)
+    },
+    {
+        path: '**',
+        redirectTo: 'index'
+    }
+];
