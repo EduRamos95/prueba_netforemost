71d8a68a36d0f61485f634d52976f60bd9569280
EduRamos95 <eduramos08.30@gmail.com>
2024-07-22 23:25:39 -0500

    create: detail component

--- test_NetForemost/src/app/views/pages/details/details.component.ts
+++ test_NetForemost/src/app/views/pages/details/details.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DetailsService } from '../../../services/details/details.service';
+import { Meal_Details } from '../../../models/the-meal-db/mealById.models';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
+
+  public mealD:Meal_Details | null = null;
+
+  constructor(
+    private router: Router,
+    private detailService: DetailsService,
+  ){}
+
+  ngOnInit(): void {
+    this.detailService.dataToSidebar$.subscribe(data => {
+      this.mealD = data;
+    });
+  }
+
+  toBack() {
+    // navegar de pagina
+    this.router.navigateByUrl("/index");
+  }
 
 }
